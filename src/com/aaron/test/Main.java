62da12b520e81b7696ef18b1ddb4ad35e9556ae3
tpk <1554696665@qq.com>
2019-08-13 12:49:05 +0800

    hi

---
+++ src/com/aaron/test/Main.java
@@ -0,0 +1,14 @@
+package com.aaron.test;
+
+/**
+ * @author tianpanke
+ * @title: Main
+ * @projectName TestGitBranch
+ * @description:
+ * @date 2019/8/13 12:47
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi Git");
+    }
+}
